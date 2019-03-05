9df82a58c814f86ac884da95935ad598cc4d8a47
Aaron Williamson <aaron@copiesofcopies.org>
2019-03-04 21:32:36 -0800

    Update YAML renderer
    Update unyaml.py to output asciidoc format; fix YAML bugs in a few license files; add asciidoc rendering.
    
    Still todo: support for seeAlso.

--- unyaml.py
+++ unyaml.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python
 # SPDX-License-Identifier: Apache-2.0
 
-#!/usr/bin/env python
-import sys
+import sys, os
 import argparse
 from yaml import load, load_all, dump
 try:
@@ -9,42 +9,42 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-parser = argparse.ArgumentParser(description='Process a license compliance info YAML file into GitHub-flavored Markdown.')
-parser.add_argument('inputfile', help='The filename of the input YAML file.')
-parser.add_argument('outputfile', help='The filename of the output Markdown file.')
+parser = argparse.ArgumentParser(description='Process a folder of FOSS license compliance info YAML files into AsciiDoc.')
+parser.add_argument('inputpath', help='The path containing the input YAML files.')
+parser.add_argument('outputfile', help='The filename of the output AsciiDoc file.')
 
 args = parser.parse_args()
 
-nf = open(args.inputfile, "r", encoding='utf8')
 of = open(args.outputfile, 'w', encoding='utf-8')
 
-licenses = load(nf, Loader=Loader)
-nf.close()
+for filename in os.listdir(args.inputpath):
+    infile = open(os.path.join(args.inputpath, filename), "r", encoding='utf8')
+    license = load(infile, Loader=Loader)
+    infile.close()
 
-for license in licenses:
+    license = license[0]
+
     if license["name"]:
-        of.write("# " + license["name"] + "\n")
+        of.write("= " + license["name"] + "\n")
     if license["licenseId"]:
         if isinstance(license["licenseId"], list):
-            of.write("- SPDX License ID:\n")
+            of.write("* SPDX License ID:\n")
             for id in license["licenseId"]:
-                of.write("  - " + id + "\n")
+                of.write("** " + id + "\n")
         else:
-            of.write("- SPDX License ID: " + license["licenseId"] + "\n")
+            of.write("* SPDX License ID: " + license["licenseId"] + "\n")
     if license["notes"]:
-        of.write("- Notes: "+ license["notes"] + "\n")
+        of.write("* Notes: "+ license["notes"] + "\n")
+    of.write("\n")
 
-    requirements = []
-    restrictions = []
+    conditions = []
     other = []
     termination = []
     lvers = []
 
     for term in license["terms"]:
-        if term["type"] == "requirement":
-            requirements.append(term)
-        if term["type"] == "restriction":
-            restrictions.append(term)
+        if term["type"] == "condition":
+            conditions.append(term)
         if term["type"] == "termination":
             termination.append(term)
         if term["type"] == "license_versions":
@@ -52,88 +52,78 @@ for license in licenses:
         if term["type"] == "other":
             other.append(term)
 
-    if len(requirements) > 0:
-        of.write("## Requirements\n")
-        of.write("|Summary|UB|MB|US|MS|Notes|\n")
-        of.write("| --- | --- | --- | --- | --- | --- |\n")
+    if len(conditions) > 0:
+        of.write("== Conditions\n")
+        of.write("[cols=6*,options=header]\n")
+        of.write("|===\n|Summary |UB |MB |US |MS |Notes\n\n")
 
-        for req in requirements:
-            of.write("|" + req["description"] + "|")
+        for req in conditions:
+            desc = req["description"]
 
             if "use_cases" in req:
+                ub = us = mb = ms = compliance_notes = ""
+
                 if "UB" in req["use_cases"]:
-                    of.write("X")
-                
-                of.write("|")
-
+                    ub = "X"
                 if "MB" in req["use_cases"]:
-                    of.write("X")
-                
-                of.write("|")
-
+                    mb = "X"
                 if "US" in req["use_cases"]:
-                    of.write("X")
-                
-                of.write("|")
-
+                    us = "X"
                 if "MS" in req["use_cases"]:
-                    of.write("X")
-                
-                of.write("|")
-            else:
-                of.write("||||")
+                    ms = "X"
 
             if "compliance_notes" in req and req["compliance_notes"]:
-                of.write(req["compliance_notes"])
+                compliance_notes = req["compliance_notes"]
                 
-            of.write("|\n")
-    if len(restrictions) > 0:
-        of.write("## Restrictions\n")
-        of.write("|Summary|Notes|\n")
-        of.write("| --- | --- |\n")
+            of.write("|{desc}\n|{ub}\n|{mb}\n|{us}\n|{ms}\n|{compliance_notes}\n"
+                     .format(desc = desc, ub = ub, mb = mb, us = us, ms = ms, compliance_notes = compliance_notes))
 
-        for res in restrictions:
-            of.write("|" + res["description"] + "|")
+        of.write("|===\n\n")
 
-            if "compliance_notes" in res and res["compliance_notes"]:
-                of.write(res["compliance_notes"])
-
-            of.write("|\n")
     if len(termination) > 0:
-        of.write("## Termination Provisions\n")
-        of.write("|Summary|Notes|\n")
-        of.write("| --- | --- |\n")
+        of.write("== Termination Provisions\n")
+        of.write("[cols=2*,options=header]\n")
+        of.write("|===\n|Summary |Notes\n\n")
 
         for termterm in termination:
-            of.write("|" + termterm["description"] + "|")
+            desc = compliance_notes = ""
+            desc = termterm["description"]
 
             if "compliance_notes" in termterm and termterm["compliance_notes"]:
-                of.write(termterm["compliance_notes"])
+                compliance_notes = termterm["compliance_notes"]
 
-            of.write("|\n")
+            of.write("|{desc}\n|{compliance_notes}\n"
+                     .format(desc = desc, compliance_notes = compliance_notes))
+        of.write("|===\n\n")
     if len(lvers) > 0:
-        of.write("## License Versioning\n")
-        of.write("|Summary|Notes|\n")
-        of.write("| --- | --- |\n")
+        of.write("== License Versioning\n")
+        of.write("[cols=2*,options=header]\n")
+        of.write("|===\n|Summary |Notes\n\n")
 
         for lver in lvers:
-            of.write("|" + lver["description"] + "|")
+            desc = compliance_notes = ""
+            desc = lver["description"]
 
-            if "compliance_notes" in lvers and lvers["compliance_notes"]:
-                of.write(lver["compliance_notes"])
+            if "compliance_notes" in lver and lver["compliance_notes"]:
+                compliance_notes = lver["compliance_notes"]
 
-            of.write("|\n")
+            of.write("|{desc}\n|{compliance_notes}\n"
+                     .format(desc = desc, compliance_notes = compliance_notes))
+        of.write("|===\n\n")
     if len(other) > 0:
-        of.write("## Other Terms\n")
-        of.write("|Summary|Notes|\n")
-        of.write("| --- | --- |\n")
+        of.write("== Other Terms\n")
+        of.write("[cols=2*,options=header]\n")
+        of.write("|===\n|Summary |Notes\n")
 
         for o in other:
-            of.write("|" + o["description"] + "|")
+            desc = compliance_notes = ""
+            desc = o["description"]
 
             if "compliance_notes" in o and o["compliance_notes"]:
-                of.write(o["compliance_notes"])
+                compliance_notes = o["compliance_notes"]
 
-            of.write("|\n")
+            of.write("|{desc}\n|{compliance_notes}\n\n"
+                     .format(desc = desc, compliance_notes = compliance_notes))
+        of.write("|===\n\n")
 
 of.close()
